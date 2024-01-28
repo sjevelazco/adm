7b19a652ea2daceaa71fe84cf7f8842bcff1c9f6
AdmirJr <admircjunior@gmail.com>
2024-01-28 18:40:06 -0300

    custom_names was removed and model selection was changed

--- model_selection.R
+++ model_selection.R
@@ -4,7 +4,6 @@ model_selection <-
     performance_var <- names(hyper_combinations)
     
     mae_columns <- grep("^mae_mean",performance_var)
-    
     hyper_combinations[,mae_columns] <-  -1*hyper_combinations[,mae_columns]
     
     performance_dict <- list(
@@ -15,18 +14,26 @@ model_selection <-
       slope = grep("^slope",performance_var),
       p_dispersion = grep("^pdispersion",performance_var)
     )
-  
-    cols_idx <- unlist(performance_dict[metrics])
-    means <- performance_var[cols_idx[cols_idx %in% grep("_mean$",performance_var)]]
-    sds <- performance_var[cols_idx[cols_idx %in% grep("sd$",performance_var)]]
     
-    ranked_combinations <- hyper_combinations %>% 
-      dplyr::arrange(
-        dplyr::across(
-          dplyr::all_of(means), desc),
-        dplyr::across(
-          dplyr::all_of(sds))
-        )
-
+    cols_idx <- unlist(performance_dict[metrics])
+    perf_means <- performance_var[cols_idx[cols_idx %in% grep("_mean$",performance_var)]]
+    
+    ranked_combinations <- hyper_combinations
+    while (nrow(hyper_combinations) > 1) {
+      for (i in perf_means) {
+        hyper_combinations <- hyper_combinations[which(hyper_combinations["corr_spear_mean"] >= summary(hyper_combinations[["corr_spear_mean"]])[5]),]
+        if (nrow(hyper_combinations == 1)){
+          break
+        }      
+      }
+    }
+    
+    selected_comb <- hyper_combinations$comb_id[1]
+    
+    ranked_combinations <- ranked_combinations %>% 
+      dplyr::filter(comb_id != selected_comb)
+    
+    ranked_combinations <- bind_rows(hyper_combinations,ranked_combinations)
+    
     return(ranked_combinations)
   }
