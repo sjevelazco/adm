bb9521db0889b499a95d609a84b19ab2f3032bf5
AdmirJr <admircjunior@gmail.com>
2024-01-29 12:29:31 -0300

    now model_selection returns a list

--- tune_abund_raf.R
+++ tune_abund_raf.R
@@ -1,3 +1,16 @@
+data = data_buffer[data_buffer$species==spp[14],]
+response = "ind_ha"
+predictors = pred_var
+predictors_f = NULL
+fit_formula = NULL
+partition = "kfold"
+predict_part = FALSE
+mtry <- seq(from = 2, to = 3, by = 1)
+ntree <- seq(from = 500, to = 1000, by = 500)
+grid <- expand.grid(mtry = mtry, ntree = ntree)
+metrics = c("mae","spearman")
+verbose = FALSE
+
 #
 tune_abund_raf <-
   function(data,
@@ -26,9 +39,6 @@ tune_abund_raf <-
     
     grid$comb_id <- paste("comb_", 1:nrow(grid), sep = "")
     
-    # names(hyper_combinations) <- grid[i,"comb_id"]
-    # hyper_combinations <- dplyr::bind_rows(hyper_combinations, .id = "comb_id")
-    
     # looping the grid
     message("Searching for optimal hyperparameters...")
     hyper_combinations <- list()
@@ -55,6 +65,11 @@ tune_abund_raf <-
     ranked_combinations <- model_selection(hyper_combinations,metrics) %>%
       dplyr::left_join(grid, by = c("comb_id"="comb_id"))
     
+    for (i in 1:length(ranked_combinations)) {
+      ranked_combinations[[i]] <- ranked_combinations[[i]] %>%
+        dplyr::left_join(grid, by = c("comb_id"="comb_id"))
+    }
+    
     # fit final model
     message("Fitting the best model...")
     final_model <-
@@ -66,16 +81,16 @@ tune_abund_raf <-
         fit_formula = fit_formula,
         partition = partition,
         predict_part = predict_part,
-        mtry = ranked_combinations[[1,"mtry"]],
-        ntree = ranked_combinations[[1,"ntree"]]
+        mtry = ranked_combinations[[1]][[1,"mtry"]],
+        ntree = ranked_combinations[[1]][[1,"ntree"]]
       )
     
     message('The best model was a Random Forest with mtry = ',
-            ranked_combinations[[1,"mtry"]],
+            ranked_combinations[[1]][[1,"mtry"]],
             " and ntree = ", 
-            ranked_combinations[[1,"ntree"]])
+            ranked_combinations[[1]][[1,"ntree"]])
     
-    final_list <- append(final_model,list(ranked_combinations = ranked_combinations))
+    final_list <- c(final_model, ranked_combinations)
     
     return(final_list)
   }
