02b5f04db6a510739fc8ac0a691f84bbc7ef3db1
AdmirJr <admirjunior55@gmail.com>
2024-09-11 16:47:03 -0300

    added progress bar to tune_abund_cnn

--- R/tune_abund_cnn.R
+++ R/tune_abund_cnn.R
@@ -156,9 +156,12 @@ tune_abund_cnn <-
     # }
 
     cl <- parallel::makeCluster(n_cores)
-    doParallel::registerDoParallel(cl)
+    doSNOW::registerDoSNOW(cl)
+    pb <- utils::txtProgressBar(max = nrow(grid), style = 3)
+    progress <- function(n) utils::setTxtProgressBar(pb, n)
+    opts <- list(progress = progress)
 
-    hyper_combinations <- foreach::foreach(i = 1:nrow(grid), .export = c("fit_abund_cnn", "adm_eval", "cnn_make_samples", "croppin_hood"), .packages = c("dplyr")) %dopar% {
+    hyper_combinations <- foreach::foreach(i = 1:nrow(grid), .options.snow = opts, .export = c("fit_abund_cnn", "adm_eval", "cnn_make_samples", "croppin_hood"), .packages = c("dplyr")) %dopar% {
       model <-
         fit_abund_cnn(
           data = data,
