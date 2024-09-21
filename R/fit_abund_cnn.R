eaf79c2fb75da4f2260be91af9f97689d4ef3ebb
AdmirJr <admirjunior55@gmail.com>
2024-09-20 21:00:54 -0300

    fit output fixed

--- R/fit_abund_cnn.R
+++ R/fit_abund_cnn.R
@@ -339,7 +339,7 @@ fit_abund_cnn <-
     eval_final <- eval_partial %>%
       dplyr::group_by(model) %>%
       dplyr::summarise(
-        dplyr::across(c(corr_spear:pdisp), 
+        dplyr::across(c(mae:pdisp), 
                       list(mean = ~mean(.x, na.rm = TRUE), 
                            sd = ~sd(.x, na.rm = TRUE)
                       )), 
